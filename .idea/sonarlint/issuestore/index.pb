1fb18952f3eaa5a9b06b465fa73aff27dbd706fa
Huu Duc <huu.daohoang@ncc.asia>
2022-12-07 21:44:25 +0700

    PACKAGE

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+@
+src/STUDENT.java,b\7\b718c64be2078f65306deb5494882a9942116e14
+?
+src/NUMBER.java,b\3\b3618092ebb4a0b3022b4296fe2da92a903a1d63
+@
+src/PACKAGE.java,9\f\9f77f6dd73a2714adf1409a2cd010dccfbd88cde
--- src/STUDENT.java
+++ src/STUDENT.java
@@ -4,19 +4,6 @@ import java.util.Scanner;
 
 public class STUDENT {
 
-    public static long factorial(int n) {
-        long fact = 1;
-        for (int i = 2; i <= n; i++) {
-            fact = fact * i;
-        }
-        return fact;
-    }
-
-    public static long tinhTohop(int k,int n){
-
-        return factorial(n)/(factorial(k)*factorial(n-k));
-
-    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -31,21 +18,7 @@ public class STUDENT {
                 count+=1;
                 map.put(rank, count);
             } else map.put(rank, 1);
-            System.out.println(rank+" ------ "+map.get(rank));
         }
-        for (int i = 0; i < n; i++) {
-            Map<Integer,Integer> temp = new HashMap<>();
-            temp.putAll(map);
 
-            Integer rank = Integer.valueOf(student[i]);
-            Integer count = temp.get(rank);
-            count-=1;
-            temp.put(rank, count);
-            int result = 0;
-            for (Map.Entry<Integer, Integer> item : temp.entrySet()) {
-                if (item.getValue() > 1) result += tinhTohop(2,item.getValue());
-            }
-            System.out.println(result);
-        }
     }
 }
